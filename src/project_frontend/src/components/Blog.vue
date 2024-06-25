a6b11e1de8e749548d4fab1d9090aaf605b255d2
Woolioo <michucity17@gmail.com>
2024-06-25 19:39:27 +0200

    added way to list wpisy

--- src/project_frontend/src/components/Blog.vue
+++ src/project_frontend/src/components/Blog.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
+        <button @click="pobierzWpisy">refresh</button>
         I H8 Niggers
+        {{ wpisy }}
     </div>
 </template>
+
+<script>
+import { project_backend } from 'declarations/project_backend/index';
+
+export default { 
+    data() { 
+        return { 
+            wpisy: []
+        }
+    },
+    methods: { 
+        async pobierzWpisy() { 
+           this.wpisy = await project_backend.odczytaj_wpisy()
+        }
+    },
+}
+</script>
